7d39634d543b8eca3c6cad43ad48410f346a21f1
Gonky10 <nicolasgoncky@gmail.com>
2025-02-12 01:09:13 -0300

    2

--- resources/views/flyers/index.blade.php
+++ resources/views/flyers/index.blade.php
@@ -4,40 +4,37 @@
 
 @section('content')
     <div class="d-flex justify-content-center align-items-center">
-        <h1>Lista de Flyers</h1>
-        <a href="{{ route('flyers.create') }}">Crear Nuevo Flyer</a>
-
-        <table border="1">
+        <table class="table">
             <thead>
                 <tr>
-                    <th>ID</th>
-                    <th>Título</th>
-                    <th>Fecha</th>
-                    <th>Hora</th>
-                    <th>Ubicación</th>
-                    <th>Organizador</th>
-                    <th>Acciones</th>
+                    <th scope="col">#</th>
+                    <th scope="col">ID</th>
+                    <th scope="col">Título</th>
+                    <th scope="col">Fecha</th>
+                    <th scope="col">Hora</th>
+                    <th scope="col">Ubicación</th>
+                    <th scope="col">Organizador</th>
+                    <th scope="col">Acciones</th>
                 </tr>
             </thead>
             <tbody>
-                @foreach ($flyers as $flyer)
-                    <tr>
-                        <td>{{ $flyer->id }}</td>
-                        <td>{{ $flyer->title }}</td>
-                        <td>{{ $flyer->event_date }}</td>
-                        <td>{{ $flyer->event_time }}</td>
-                        <td>{{ $flyer->event_location }}</td>
-                        <td>{{ $flyer->organizer_name }}</td>
-                        <td>
-                            <a href="{{ route('flyers.edit', $flyer->id) }}">Editar</a> |
-                            <form action="{{ route('flyers.destroy', $flyer->id) }}" method="POST" style="display:inline;">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit">Eliminar</button>
-                            </form>
-                        </td>
-                    </tr>
-                @endforeach
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td colspan="2">Larry the Bird</td>
+                    <td>@twitter</td>
+                </tr>
             </tbody>
         </table>
     </div>
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -13,26 +13,23 @@
 </head>
 
 <body>
-    <div class="content">
+    <div class="container-fluid">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
         </script>
 
-        <div class="card">
-            <div class="card-header d-flex justify-content-center align-items-center">
-                <span class="p-2">Simpleado</span>
-                <img src="{{ asset('images/Simpleado.png') }}" alt="Simpleado" class="logo">
+        <div class="bg-black text-white card-header d-flex align-items-center justify-content-between pe-3 pt-3 pb-3">
+            <div class="flex-grow-1 text-center">
+                <p class="mb-0">Simpleado</p>
             </div>
+            <img src="{{ asset('images/Simpleado.png') }}" alt="Simpleado" class="logo rounded">
         </div>
-
+        <div class="container-sm bg-black">100% wide until small breakpoint</div>
 
         <main>
             @yield('content')
         </main>
 
-        <footer>
-            <p>Simpleado © {{ date('Y') }}</p>
-        </footer>
 
     </div>
 </body>
